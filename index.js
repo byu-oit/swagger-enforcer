b00530fc0c1474b7eaccd86832aaaf342437bb67
James <james.speirs@gmail.com>
2017-04-17 16:13:19 -0600

    add release interface

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const enforcer              = require('./bin/enforcer');
 enforcer.injectParameters   = require('./bin/inject-parameters');
 enforcer.is                 = require('./bin/is');
+enforcer.release            = require('./bin/release');
 enforcer.same               = require('./bin/same');
 enforcer.to                 = require('./bin/convert-to');
