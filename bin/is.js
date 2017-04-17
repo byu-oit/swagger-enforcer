8cfff3a83951a142c2ed6779e67766387b688371
James <james.speirs@gmail.com>
2017-04-17 15:37:05 -0600

    rename time to dateTime

--- bin/is.js
+++ bin/is.js
@@ -5,10 +5,12 @@ exports.base64 = rx.base64.test;
 
 exports.binary = rx.binary.test;
 
+exports.boolean = rx.boolean.test;
+
 exports.date = rx.date.test;
+
+exports.dateTime = rx.time.test;
 
 exports.integer = rx.integer.test;
 
 exports.number = rx.number.test;
-
-exports.dateTime = rx.time.test;
