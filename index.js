c26957165540bc0b007ca46b98078d89578fc0c2
James <james.speirs@gmail.com>
2017-04-17 15:37:23 -0600

    add inject params

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const enforcer          = require('./bin/enforcer');
-enforcer.is             = require('./bin/is');
-enforcer.same           = require('./bin/same');
-enforcer.to             = require('./bin/convert-to');
+const enforcer              = require('./bin/enforcer');
+enforcer.injectParameters   = require('./bin/inject-parameters');
+enforcer.is                 = require('./bin/is');
+enforcer.same               = require('./bin/same');
+enforcer.to                 = require('./bin/convert-to');
