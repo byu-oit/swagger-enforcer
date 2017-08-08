23d985ab173042269b15b1451141d504e3ed7f1e
James <james.speirs@gmail.com>
2017-08-08 16:10:01 -0600

    test mutate true

--- test/inject-parameters.js
+++ test/inject-parameters.js
@@ -36,31 +36,13 @@ describe('inject parameters', () => {
     });
 
     it('colon replacement', () => {
-        const value = injector(':name is :age years old :when', { name: 'Bob', age: 12, when: 'today' }, { replacement: 'colon' });
+        const value = injector(':name is :age years old :when', { name: 'Bob', age: 12, when: 'today' }, { replacement: 'colon', mutate: true });
         expect(value).to.equal('Bob is 12 years old today');
     });
 
     it('double handlebar replacement', () => {
         const value = injector('{{name}} is {{age}} years old {{when}}', { name: 'Bob', age: 12, when: 'today' }, { replacement: 'doubleHandlebar' });
         expect(value).to.equal('Bob is 12 years old today');
-    });
-
-    it('array replacement', () => {
-        const value = [
-            '{name} is {age} years old {when}',
-            '{age} is the age of {name} as of {when}'
-        ];
-        const result = injector(value, { name: 'Bob', age: 12, when: 'today' });
-        expect(result).to.deep.equal(['Bob is 12 years old today', '12 is the age of Bob as of today']);
-    });
-
-    it('object replacement', () => {
-        const value = {
-            foo: '{name} is {age} years old {when}',
-            bar: '{age} is the age of {name} as of {when}'
-        };
-        const result = injector(value, { name: 'Bob', age: 12, when: 'today' });
-        expect(result).to.deep.equal({ foo: 'Bob is 12 years old today', bar: '12 is the age of Bob as of today' });
     });
 
     it('custom replace', () => {
@@ -71,6 +53,60 @@ describe('inject parameters', () => {
         };
         const value = injector('{name} is {age} years old {when}', { name: 'Bob', age: 12, when: 'today' }, options);
         expect(value).to.equal("I'm a little teapot.");
+    });
+
+    describe('immutable', () => {
+        it('array replacement', () => {
+            const value = [
+                '{name} is {age} years old {when}',
+                '{age} is the age of {name} as of {when}',
+                0
+            ];
+            const result = injector(value, { name: 'Bob', age: 12, when: 'today' }, { mutate: false });
+            expect(result).to.deep.equal(['Bob is 12 years old today', '12 is the age of Bob as of today', 0]);
+            expect(result).not.to.equal(value);
+        });
+
+        it('object replacement', () => {
+            const value = {
+                foo: '{name} is {age} years old {when}',
+                bar: '{age} is the age of {name} as of {when}',
+                num: 0
+            };
+            const result = injector(value, { name: 'Bob', age: 12, when: 'today' }, { mutate: false });
+            expect(result).to.deep.equal({ foo: 'Bob is 12 years old today', bar: '12 is the age of Bob as of today', num: 0 });
+            expect(result).not.to.equal(value);
+        });
+    });
+
+    describe('mutable', () => {
+        it('array replacement', () => {
+            const value = [
+                '{name} is {age} years old {when}',
+                '{age} is the age of {name} as of {when}',
+                0,
+                'nothing to replace',
+                {}
+            ];
+            const result = injector(value, { name: 'Bob', age: 12, when: 'today' }, { mutate: true });
+            expect(result).to.deep.equal(['Bob is 12 years old today', '12 is the age of Bob as of today', 0, 'nothing to replace', {}]);
+            expect(result).to.equal(value);
+            expect(result[3]).to.equal(value[3]);
+        });
+
+        it('object replacement', () => {
+            const value = {
+                foo: '{name} is {age} years old {when}',
+                bar: '{age} is the age of {name} as of {when}',
+                num: 0,
+                abc: 'nothing to replace',
+                def: {}
+            };
+            const result = injector(value, { name: 'Bob', age: 12, when: 'today' }, { mutate: true });
+            expect(result).to.deep.equal({ foo: 'Bob is 12 years old today', bar: '12 is the age of Bob as of today', num: 0, abc: 'nothing to replace', def: {} });
+            expect(result).to.equal(value);
+            expect(result.def).to.equal(value.def);
+        });
     });
 
 });
