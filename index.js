1770400112f1952e8a8bc3c4e8aeda691be34331
James <james.speirs@gmail.com>
2017-08-14 14:22:29 -0600

    expose applyDefaults

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const enforcer              = require('./bin/enforcer');
+enforcer.applyTemplate      = require('./bin/apply-template');
 enforcer.injectParameters   = require('./bin/inject-parameters');
 enforcer.is                 = require('./bin/is');
 enforcer.release            = require('./bin/release');
