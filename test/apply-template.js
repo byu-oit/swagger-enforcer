fba5133c4a3c1b01dd3d43c7ad4b97542582794f
James <james.speirs@gmail.com>
2017-08-15 09:45:19 -0600

    add x-variable tests

--- test/apply-template.js
+++ test/apply-template.js
@@ -20,104 +20,138 @@ const expect            = require('chai').expect;
 
 describe('apply template', () => {
 
-    it('can apply string template', () => {
-        const schema = {
-            type: 'string',
-            'x-template': '{hello}'
-        };
-        const result = applyTemplate(schema, {}, { hello: 'Hi' });
-        expect(result).to.equal('Hi');
-    });
+    describe('templates', () => {
 
-    it('can apply integer template', () => {
-        const schema = {
-            type: 'integer',
-            'x-template': '{hello}'
-        };
-        const result = applyTemplate(schema, null, { hello: 15 });
-        expect(result).to.equal(15);
-    });
-
-    it('can apply number template', () => {
-        const schema = {
-            type: 'number',
-            'x-template': '{hello}'
-        };
-        const result = applyTemplate(schema, {}, { hello: 1.5 });
-        expect(result).to.equal(1.5);
-    });
-
-    it('can apply boolean template', () => {
-        const schema = {
-            type: 'boolean',
-            'x-template': '{hello}'
-        };
-        const result = applyTemplate(schema, {}, { hello: true });
-        expect(result).to.equal(true);
-    });
-
-    it('can apply byte template', () => {
-        const schema = {
-            type: 'string',
-            format: 'byte',
-            'x-template': '{x}'
-        };
-        const result = applyTemplate(schema, {}, { x: 'Hello' });
-        expect(result).to.equal('SGVsbG8=');
-    });
-
-    // TODO: figure out how to evaluate as number
-    /*it.only('can apply binary template', () => {
-        const schema = {
-            type: 'string',
-            format: 'binary',
-            'x-template': '{x}'
-        };
-        const result = applyTemplate(schema, {}, { x: 1 });
-        expect(result).to.equal('00000001');
-    });*/
-
-    // TODO: figure out how to evaluate as date
-    /*it('can apply date template', () => {
-        const schema = {
-            type: 'string',
-            format: 'date',
-            'x-template': '{x}'
-        };
-        const result = applyTemplate(schema, {}, { x: new Date('2000-01-01T00:00:00.000Z') });
-        expect(result).to.equal('2000-01-01T00:00:00.000Z');
-    });*/
-
-
-    it('will not apply x-template without param', () => {
-        const schema = {
-            type: 'integer',
-            'x-template': '{hello}'
-        };
-        const result = applyTemplate(schema, {}, {});
-        expect(result).to.equal(undefined);
-    });
-
-    it('will apply default', () => {
-        const schema = {
-            type: 'integer',
-            default: 5
-        };
-        const result = applyTemplate(schema, {}, {});
-        expect(result).to.equal(5);
-    });
-
-    it('will use custom replacement', () => {
-        const schema = {
-            type: 'string',
-            'x-template': 'Hello'
-        };
-        const result = applyTemplate(schema, {}, {}, {
-            replacement: function(value, param) {
-                return 'abc';
-            }
+        it('can apply string template', () => {
+            const schema = {
+                type: 'string',
+                'x-template': '{hello}'
+            };
+            const result = applyTemplate(schema, {}, {hello: 'Hi'});
+            expect(result).to.equal('Hi');
         });
-        expect(result).to.equal('abc');
+
+        it('can apply integer template', () => {
+            const schema = {
+                type: 'integer',
+                'x-template': '{hello}'
+            };
+            const result = applyTemplate(schema, null, {hello: 15});
+            expect(result).to.equal('15');
+        });
+
+        it('can apply number template', () => {
+            const schema = {
+                type: 'number',
+                'x-template': '{hello}'
+            };
+            const result = applyTemplate(schema, {}, {hello: 1.5});
+            expect(result).to.equal('1.5');
+        });
+
+        it('can apply boolean template', () => {
+            const schema = {
+                type: 'boolean',
+                'x-template': '{hello}'
+            };
+            const result = applyTemplate(schema, {}, {hello: true});
+            expect(result).to.equal('true');
+        });
+
+        it('can apply byte template', () => {
+            const schema = {
+                type: 'string',
+                format: 'byte',
+                'x-template': '{x}'
+            };
+            const result = applyTemplate(schema, {}, {x: 'Hello'});
+            expect(result).to.equal('Hello');
+        });
+
+        it('will not apply x-template without param', () => {
+            const schema = {
+                type: 'integer',
+                'x-template': '{hello}'
+            };
+            const result = applyTemplate(schema, {}, {});
+            expect(result).to.equal(undefined);
+        });
+
+        it('will apply default', () => {
+            const schema = {
+                type: 'integer',
+                default: 5
+            };
+            const result = applyTemplate(schema, {}, {});
+            expect(result).to.equal(5);
+        });
+
+        it('will use custom replacement', () => {
+            const schema = {
+                type: 'string',
+                'x-template': '{first} {second}'
+            };
+            const result = applyTemplate(schema, {}, {}, {
+                replacement: function (value, param) {
+                    return 'abc';
+                }
+            });
+            expect(result).to.equal('abc');
+        });
+    });
+
+    describe('variables', () => {
+
+        it('can apply binary variable', () => {
+            const schema = {
+                type: 'string',
+                format: 'binary',
+                'x-variable': 'x'
+            };
+            const result = applyTemplate(schema, {}, { x: 1 });
+            expect(result).to.equal('00000001');
+        });
+
+        it('can apply byte variable', () => {
+            const schema = {
+                type: 'string',
+                format: 'byte',
+                'x-variable': 'x'
+            };
+            const result = applyTemplate(schema, {}, {x: 'Hello'});
+            expect(result).to.equal('SGVsbG8=');
+        });
+
+        it('can apply date template', () => {
+            const schema = {
+                type: 'string',
+                format: 'date',
+                'x-variable': 'x'
+            };
+            const result = applyTemplate(schema, {}, { x: new Date('2000-01-01T00:00:00.000Z') });
+            expect(result).to.equal('2000-01-01');
+        });
+
+        it('can apply date-time template', () => {
+            const schema = {
+                type: 'string',
+                format: 'date-time',
+                'x-variable': 'x'
+            };
+            const result = applyTemplate(schema, {}, { x: new Date('2000-01-01T00:00:00.000Z') });
+            expect(result).to.equal('2000-01-01T00:00:00.000Z');
+        });
+
+        it('can apply object', () => {
+            const schema = {
+                type: 'object',
+                'x-variable': 'x'
+            };
+            const o = { a: true };
+            const result = applyTemplate(schema, {}, { x: o });
+            expect(result).to.deep.equal(o);
+        });
+
     });
 
 });
