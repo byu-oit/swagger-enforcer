a2d51284f123a974520f7dcc9efe0b35a3c70e0d
James Speirs <james.speirs@gmail.com>
2017-10-23 12:01:41 -0600

    numbers and integers can be negative

--- bin-2/rx.js
+++ bin-2/rx.js
@@ -21,5 +21,5 @@ exports.boolean =   /^(?:true|false)$/;
 exports.byte =      /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
 exports.date =      /^(\d{4})-(\d{2})-(\d{2})$/;
 exports.dateTime =  /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d{3}))?Z$/;
-exports.integer =   /^\d+$/;
-exports.number =    /^\d+(?:\.\d+)?$/;
+exports.integer =   /^-?\d+$/;
+exports.number =    /^-?\d+(?:\.\d+)?$/;
