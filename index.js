fac2c4300a85e3e382626f3b500496a6e4049713
James Speirs <james.speirs@gmail.com>
2017-10-23 09:09:14 -0600

    allow multiple schemas

--- index.js
+++ index.js
@@ -3,20 +3,40 @@ const parser        = require('swagger-parser');
 const Swagger       = require('./bin-2/swagger');
 
 /**
- * Load a swagger enforcer instance.
+ * Load a swagger enforcer instance for a custom spec.
+ * @param {object} functions The custom spec functions.
+ * @param {string|object} definition
+ * @param {object} [options]
+ * @returns {Promise.<Swagger>}
+ */
+exports.custom = function(functions, definition, options) {
+    return parser.validate(definition)
+        .then(schema => {
+            return new Swagger(functions, schema, options);
+        });
+};
+
+/**
+ * Load a swagger enforcer instance for spec version 2.0.
  * @param {string|object} definition
  * @param {object} [options]
  * @returns {Promise.<Swagger>}
  */
 exports.v2 = function(definition, options) {
-    return parser.validate(definition)
-        .then(schema => {
-            return new Swagger(2, definition, options);
-        });
+    const functions = require('./bin-2/versions/v2');
+    return exports.custom(functions, definition, options);
 };
 
+/**
+ * Load a swagger enforcer instance for spec version 3.0.0.
+ * @param {string|object} definition
+ * @param {object} [options]
+ * @returns {Promise.<Swagger>}
+ */
 exports.v3 = function(definition, options) {
     throw Error('Swagger v3 not yet implemented');
+    const functions = require('./bin-2/versions/v3');
+    return exports.custom(functions, definition, options);
 };
 
 /*
